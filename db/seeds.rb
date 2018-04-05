a4ef877effbac8336abab728cb36e0a492fc8106
Anh Viet <virt@vinova.sg>
2018-04-05 16:41:28 +0700

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,43 +8,43 @@
 
 
 require 'faker'
-
-Member.destroy_all
+require 'friendly_id'
 puts "Creat Admin"
+Member.destroy_all
 member = Member.new
 member.first_name = "admin"
 member.last_name = "book"
-member.user_name = "admin"
+member.user_name = "lucky"
 member.email = "admin@gmail.com"
 member.password = 123456789
 member.gender = 1
-member.save
+member.save!
 puts "Creat User"
 (1..20).each do |i|
 	member = Member.new
 	member.first_name = Faker::Name.name
 	member.last_name = Faker::Name.name
-	member.user_name = "admin"
+	member.user_name = Faker::Name.name.remove(" ")
 	member.email = Faker::Name.name.remove(" ")+"@gmail.com"
 	member.password = 123456789
 	member.gender = 1
-	member.save
+	member.save!
 end
 
 puts "Create Category"
-
-(1..20).each do |i|
+Category.destroy_all
+(1..10).each do |i|
 	category = Category.new
-	category.name = Faker::Science.scientist
-	category.save	
+	category.name = Faker::Music.chord
+	category.save!	
 end
-
 puts "Create Book"
-(1..100).each do |i|
+Book.destroy_all
+(1..50).each do |i|
 	book = Book.new
 	book.year = 1900 + i
 	book.name = Faker::Book.title
 	book.description = Faker::Book.publisher
-	book.category = i%20
-	book.save
+	book.category_id = i%10  > 0 ? i%10 : 1
+	book.save!
 end
